862aef6d65fa2184ea6167ff523322c1d817bcb5
luisdacanto <luisdcanto@comunidad.unam.mx>
2025-03-03 23:23:42 -0600

    caminata clasica

---
+++ classicrandomwalk.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+n_p=10000
+n_c=10000
+posiciones_finales=[]
+
+for i in range(n_c):
+    posicion= 0
+    for _ in range(n_p):
+        moneda = random.choice([-1,1])
+        posicion+=moneda
+    posiciones_finales.append(posicion)
+
+plt.hist(posiciones_finales, bins=50, density=True, alpha=0.6, color='b')
+plt.title('Distribución de la Caminata Aleatoria')
+plt.xlabel('Posición')
+plt.ylabel('Densidad de Probabilidad')
+plt.grid(True)
+plt.show()
