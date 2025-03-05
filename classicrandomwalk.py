3dcb64ad9e60f13992f323ab086d599e860297f1
luisdacanto <luisdcanto@comunidad.unam.mx>
2025-03-05 00:15:04 -0600

    5/03/25

--- classicrandomwalk.py
+++ classicrandomwalk.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-n_p=10000
+n_p=1000
 n_c=10000
 posiciones_finales=[]
 
 for i in range(n_c):
     posicion= 0
     for _ in range(n_p):
-        moneda = random.choice([-1,1])
-        posicion+=moneda
+        movimiento = random.choice([-1,1])
+        posicion+=movimiento
     posiciones_finales.append(posicion)
 
 plt.hist(posiciones_finales, bins=50, density=True, alpha=0.6, color='b')
---
+++ quantumwalk.py
@@ -0,0 +1,18 @@
+import hiperwalk as hw
+import numpy as np
+n=200
+line=hw.Line(n)
+
+
+qw=hw.Coined(line, coin= "hadamard")
+
+v=n//2
+
+state_i=(qw.ket((v,v+1))+1j*qw.ket((v,v-1)))/np.sqrt(2)
+
+
+state_f=qw.simulate(range=(n//2,n//2+1),state=state_i)
+
+prob = qw.probability_distribution(state_f)
+
+hw.plot_probability_distribution(prob,plot="line") 
