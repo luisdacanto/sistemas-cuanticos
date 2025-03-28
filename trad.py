5a7317fcc7dca6f7fe44b1e608fa775bdb767c9b
luisdacanto <luisdcanto@comunidad.unam.mx>
2025-03-28 16:08:13 -0600

    28/03/2025

--- trad.py
+++ trad.py
@@ -7,22 +7,26 @@ H = (1/np.sqrt(2)) * np.array([[1, 1], [1, -1]])
 H_sparse = sparse.csr_matrix(H)
 
 def coin(t):
-    sparse.kron(sparse.identity(t),H_sparse)
+     return sparse.kron(sparse.identity(2*t+1),H_sparse)
 
 def shift(t):
-    forward = np.pad(np.eye(t), ((1, 0), (0, 1)))  # paso adelante
-    backward = np.pad(np.eye(t), ((0, 1), (1, 0)))  # paso atras
+    forward = np.pad(np.eye(2*t), ((1, 0), (0, 1)))  # paso adelante
+    backward = np.pad(np.eye(2*t), ((0, 1), (1, 0)))  # paso atras
 
     return (
         sparse.kron(forward, np.array([[1, 0], [0, 0]])) +
         sparse.kron(backward, np.array([[0, 0], [0, 1]]))
     )
-S=shift(3)
-print(S.toarray())
-
-def evolution(t):
-    return shift(t)*coin(t)
+def evolution_step(t):
+    return shift(t)@coin(t)
 
 #Cálculos
+psi=np.pad(np.array([1, 1]) / np.sqrt(2),2) #estado inicial
 
+#Caminata Cuántica
 
+for t in range(7):
+    psi=evolution_step(t).dot(psi)
+    psi=np.pad(psi,(2,2))
+
+print(psi)
